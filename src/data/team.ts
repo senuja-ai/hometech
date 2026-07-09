56245f832e43904e026609984f99b6a1b77de796
Senuja Ganganath <senujaganganath@gmail.com>
2026-07-09 17:12:56 +0200

    modified:   src/data/team.ts 	modified:   package-lock.json

--- src/data/team.ts
+++ src/data/team.ts
@@ -33,8 +33,8 @@ export const teamMembers: TeamMember[] = [
     name: 'Sewwandi',
     photo: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=400',
     role: {
-      en: 'Lead IT Technician',
-      da: 'Ledende IT-tekniker',
+      en: 'support IT Technician',
+      da: 'support IT-tekniker',
     },
     bio: {
       en: 'Sewwandi is a skilled IT professional with a hands-on approach from computer hardware, to tech support. Certified in Windows tech support and programming, she has a talent for explaining complex tech in plain language.',
@@ -45,12 +45,24 @@ export const teamMembers: TeamMember[] = [
     name: 'Anuhas',
     photo: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=400',
     role: {
-      en: 'Lead IT Technician',
-      da: 'Ledende IT-tekniker',
+      en: 'support IT Technician',
+      da: 'support IT-tekniker',
     },
     bio: {
       en: 'Anuhas is a skilled IT professional with a hands-on approach from computer hardware, to tech support. Certified in Windows tech support and programming, he has a talent for explaining complex tech in plain language.',
       da: 'Anuhas er en dygtig IT-professionel med en praktisk tilgang til alt fra computerhardware til teknisk support. Han er certificeret i Windows teknisk support og programmering og har et talent for at forklare kompleks teknologi på et letforståeligt sprog.',
+    },
+  },
+  {
+    name: 'Selini',
+    photo: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=400',
+    role: {
+      en: 'support IT Technician',
+      da: 'support IT-tekniker',
+    },
+    bio: {
+      en: 'Selini is a skilled IT professional with a hands-on approach from computer hardware, to tech support. Certified in Windows tech support and programming, she has a talent for explaining complex tech in plain language.',
+      da: 'Selini er en dygtig IT-professionel med en praktisk tilgang til alt fra computerhardware til teknisk support. Hun er certificeret i Windows teknisk support og programmering og har et talent for at forklare kompleks teknologi på et letforståeligt sprog.',
     },
   },
 ];
